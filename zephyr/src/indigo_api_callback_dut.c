153d2115fa252b921a3394e4b77bf6db18720569
Triveni Danda <triveni.danda@nordicsemi.no>
2023-09-21 10:47:17 +0530

    [nrf noup]: zephyr: qt: Reduce buffer size
    Adjust the buffer sizes from 256 to 32 to optimize the
    stack size. 32 bytes is sufficient to store the values
    in the `reset_device_handler` function.
    
    Signed-off-by: Triveni Danda <triveni.danda@nordicsemi.no>

--- zephyr/src/indigo_api_callback_dut.c
+++ zephyr/src/indigo_api_callback_dut.c
@@ -131,7 +131,7 @@ static int get_control_app_handler(struct packet_wrapper *req, struct packet_wra
 static int reset_device_handler(struct packet_wrapper *req, struct packet_wrapper *resp) {
     int status = TLV_VALUE_STATUS_NOT_OK;
     char *message = TLV_VALUE_RESET_NOT_OK;
-    char role[TLV_VALUE_SIZE], log_level[TLV_VALUE_SIZE], band[TLV_VALUE_SIZE];
+    char role[32], log_level[32], band[32];
     struct tlv_hdr *tlv = NULL;
 
     /* TLV: ROLE */
